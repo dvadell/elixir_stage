20e6889353c26a139cd271421f82e6d10f39af8b
Diego M. Vadell <diego.m.vadell@gmail.com>
2026-08-01 15:39:41 +0000

    Fix format

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,7 @@ config :soundpanel, SoundpanelWeb.Endpoint,
 # Force using SSL in production. This also sets the "strict-security-transport" header,
 # known as HSTS. If you have a health check endpoint, you may want to exclude it below.
 # Note `:force_ssl` is required to be set at compile-time.
-config :soundpanel, SoundpanelWeb.Endpoint,
-  force_ssl: false
+config :soundpanel, SoundpanelWeb.Endpoint, force_ssl: false
 
 # Do not print debug messages in production
 config :logger, level: :info
