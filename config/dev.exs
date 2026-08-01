82e007b1bbab85f16736a13ce5046f52b97e3f19
Diego M. Vadell <diego.m.vadell@gmail.com>
2026-08-01 12:15:41 +0000

    Make soundpanel listen on port 4001

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :soundpanel, Soundpanel.Repo,
 config :soundpanel, SoundpanelWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}],
+  http: [ip: {0, 0, 0, 0}, port: 4001],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
