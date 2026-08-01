84ffa333fccf0e5221d90207f2fc021037055068
Diego M. Vadell <diego.m.vadell@gmail.com>
2026-08-01 15:36:34 +0000

    Remove ssl

--- apps/soundpanel/lib/soundpanel_web/controllers/page_controller.ex
+++ apps/soundpanel/lib/soundpanel_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule SoundpanelWeb.PageController do
   use SoundpanelWeb, :controller
 
   def home(conn, _params) do
-    redirect(conn, to: ~p"/soundboard.html")
+    redirect(conn, to: "/soundboard.html")
   end
 end
